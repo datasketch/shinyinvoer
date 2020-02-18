4fab97da96989706a8843f23aeb8ea84b97a9a1c
Wim <wfjvdham@gmail.com>
2020-02-18 10:45:26 -0500

    Add remove buttons and logic

--- inst/lib/colorPicker/colorInput-bindings.js
+++ inst/lib/colorPicker/colorInput-bindings.js
@@ -1,49 +1,93 @@
 var colorInputBinding = new Shiny.InputBinding();
-var state = []
+var state = [];
 var handleInputColorChange = function (event) {
-  var index = this.dataset.index;
-  var color = event.target.value;
-  this.value = color;
-  var inputIndex = state.findIndex(function (item) {
-    return item.id === Number(index)
+  var input = event.target;
+  var index = parseInt(input.dataset.index);
+
+  // Remove old value from state
+  state = state.filter(function(color) {
+    return color.id != index;
   });
-  // Actualiza el estado
-  state.splice(inputIndex, 1, Object.assign(state[inputIndex], { color: color }));
-}
+
+  // Add new value to state
+  state.push({
+    id: index,
+    input: input,
+    color: input.value
+  });
+  state.sort(function(a, b){return a.id - b.id});
+};
+
+var remove_color = function(event) {
+  var index = parseInt(event.target.getAttribute("index"));
+
+  // Remove from state
+  state = state.filter(function(color) {
+    return color.id != index;
+  });
+
+  // Remove from DOM
+  var div = event.target.parentNode;
+  div.remove();
+};
 
 $.extend(colorInputBinding, {
   find: function(scope) {
 		// Encuentra el elemento que se ha creado
-
-	return $(scope).find(".input-color-palette");
+	  return $(scope).find(".input-color-palette");
   },
   initialize: function(el){
     $(el).find('input').each(function (index, input) {
       // Guardar referencia al id en el elemento
       input.dataset.index = index;
       // Guardar en el estado global
-     state.push({
-       id: index,
-       input: input,
-       color: input.value
-     });
-     // Enlazar evento a input
+      state.push({
+        id: index,
+        input: input,
+        color: input.value
+      });
+      state.sort(function(a, b){return a.id - b.id});
+
       $(input).on('change', handleInputColorChange);
     });
 
     // Enlazar boton para agregar color
     $(el).find('#add-color').click(function () {
+      var div = document.createElement('div');
+      div.setAttribute('class', "input-color-container");
+      var index = state[state.length-1].id + 1;
+
+      //create color picker
       var input = document.createElement('input');
       input.setAttribute('type', 'color');
       input.value = '#FFFFFF';
-      el.insertBefore(input, this)
+      input.dataset.index = index;
+
+      // call handleInputColorChange to update state
+      handleInputColorChange({'target': input});
+      $(input).on('change', handleInputColorChange);
+
+      // create remove color button
+      var button = document.createElement('button');
+      button.setAttribute('index', index);
+      button.innerHTML = "x";
+      button.onclick = remove_color;
+      div.appendChild(input);
+      div.appendChild(button);
+      el.insertBefore(div, this);
+    });
+
+    // Add remove_color onclick top all except the add-color button
+    $(el).find('button').each(function(index, button) {
+      if (button.id !== "add-color") {
+        button.onclick = remove_color;
+      }
     });
   },
   getValue: function(el) {
-
     return state.map(item => item.color);
   },
-   setValue: function(el, value) {
+  setValue: function(el, value) {
    //console.log(el)
   },
   subscribe: function(el, callback) {
@@ -53,21 +97,13 @@ $.extend(colorInputBinding, {
        if (input.dataset.index) {
          return; // early return
        }
-       // Agregar data-index
-       input.dataset.index = index;
-       // Guardar en el estado global
-      state.push({
-        id: index,
-        input: input,
-        color: input.value
-      });
-      $(input).on('change', handleInputColorChange);
+       $(input).on('change', handleInputColorChange);
      });
      callback();
-   })
+   });
   },
   unsubscribe: function(el) {
   }
 });
 
-Shiny.inputBindings.register(colorInputBinding, 'shiny.colorInputBinding')
+Shiny.inputBindings.register(colorInputBinding, 'shiny.colorInputBinding');
