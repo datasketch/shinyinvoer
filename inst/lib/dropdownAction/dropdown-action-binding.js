8d7810bf1ed642036b9d49df6b3c49ad932f5011
brazadas <anaherand@hotmail.com>
2020-07-02 09:18:06 -0500

    dropdownActionInput with images and styles when not downloadable

--- inst/lib/dropdownAction/dropdown-action-binding.js
+++ inst/lib/dropdownAction/dropdown-action-binding.js
@@ -10,18 +10,48 @@ function dropdownActionCreateTriggerButton(label) {
   return button;
 }
 
-function dropdownActionCreateActionList(choices) {
-  const dropdownActionList = document.createElement('ul');
+function dropdownActionCreateActionList(choices, downloadable, loader, done) {
+  const dropdownActionList = document.createElement('div');
   dropdownActionList.classList.add('dropdown-action-list');
 
-  choices.forEach(function (choice) {
-    const dropdownActionItem = document.createElement('li');
-    dropdownActionItem.classList.add('dropdown-action-item');
-    dropdownActionItem.dataset.action = choice.id;
-    dropdownActionItem.textContent = choice.label;
-    dropdownActionList.appendChild(dropdownActionItem);
-  });
+  const isdownloadable = downloadable == 'TRUE'
 
+  if (isdownloadable) {
+    choices.forEach(function (choice) {
+    //choices.forEach(function (choice, index) {
+      const dropdownActionItem = document.createElement('a');
+      dropdownActionItem.setAttribute('id', choice.id);
+      dropdownActionItem.setAttribute('target', '_blank');
+      dropdownActionItem.classList.add('shiny-download-link', 'dropdown-action-item');
+      dropdownActionItem.setAttribute('download', '');
+      dropdownActionItem.dataset.action = choice.id;
+      var dropdownActionItemsImage = '<i class="fa fa-download"></i>';
+      if (choice.image !== "") {
+        dropdownActionItemsImage = '<img src="' + choice.image + '">';
+      }
+      const dropdownActionItems = '<div style="align-items: center; display: flex; justify-content: space-between;"><div>' + dropdownActionItemsImage + choice.label + '</div><div><img src="' + loader + '"></div></div>';
+      dropdownActionItem.innerHTML = dropdownActionItems;
+      dropdownActionList.appendChild(dropdownActionItem);
+    });
+  } else {
+    choices.forEach(function (choice) {
+      const dropdownActionItem = document.createElement('div');
+      dropdownActionItem.setAttribute('title', choice.label);
+      dropdownActionItem.classList.add('dropdown-action-item');
+      dropdownActionItem.dataset.action = choice.id;
+      if (choice.image) {
+        const dropdownActionItemImage = document.createElement('img');
+        dropdownActionItemImage.classList.add('dropdown-action-item-image');
+        dropdownActionItemImage.setAttribute('src', choice.image);
+        dropdownActionItem.appendChild(dropdownActionItemImage);
+      }
+      const dropdownActionItemLabel = document.createElement('span');
+      dropdownActionItemLabel.classList.add('dropdown-action-item-label');
+      dropdownActionItemLabel.textContent = choice.label;
+      dropdownActionItem.appendChild(dropdownActionItemLabel);
+      dropdownActionList.appendChild(dropdownActionItem);
+    });
+  }
   return dropdownActionList;
 }
 
@@ -35,7 +65,7 @@ $.extend(binding, {
       el.dataset.label
     );
     const dropdownActionList = dropdownActionCreateActionList(
-      JSON.parse(el.dataset.options)
+      JSON.parse(el.dataset.options), el.dataset.downloadable, el.dataset.loader, el.dataset.done
     );
     el.appendChild(dropdownActionTrigger);
     el.appendChild(dropdownActionList);
@@ -59,16 +89,16 @@ $.extend(binding, {
     }
 
     el.addEventListener('click', function (event) {
-      const { target } = event;
-      if (
-        target.matches('.dropdown-action-trigger') ||
-        target === dropdownActionIndicator
-      ) {
-        dropdownActionToggleStates();
-        return callback();
-      }
+        const { target } = event;
+        if (
+          target.matches('.dropdown-action-trigger') ||
+          target === dropdownActionIndicator
+        ) {
+          dropdownActionToggleStates();
+          return callback();
+        }
 
-      if (target.matches('.dropdown-action-item')) {
+        if (target.matches('.dropdown-action-item')) {
         const action = target.dataset.action;
         el.dataset.selected = action || '';
         dropdownActionToggleStates();
