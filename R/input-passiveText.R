338176ef6372f426f2d74f0d8869125495ea5d01
David Daza <03dazal@gmail.com>
2021-02-18 10:11:25 -0500

    fix error #60

--- R/input-passiveText.R
+++ R/input-passiveText.R
@@ -2,7 +2,7 @@
 passiveTextInput <-
   function(inputId,
            label = NULL,
-           value = ,
+           value = "",
            placeholder = NULL,
            showButton = F,
            buttonLabel = NULL) {
